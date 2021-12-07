d88d3dd72c55993cd8dc6e09d7b1b2fb29a55c91
karyssa-dandrea <karyssadandrea@gmail.com>
2021-12-07 14:20:00 -0800

    added my embeded flags and html

--- countrycard/Countrycard.js
+++ countrycard/Countrycard.js
@@ -1,3 +1,11 @@
 import './Countrycard.css';
 
-export default function CountryCard({ })
+export default function CountryCard({ name, iso2, continent }) {
+  return (
+    <div className="Blogcard">
+      <img src={`https://flagcdn.com/16x12/${iso2.toLowerCase()}.png`} />
+      <h1>{name}</h1>
+      <h2>{continent}</h2>
+    </div>
+  );
+}
